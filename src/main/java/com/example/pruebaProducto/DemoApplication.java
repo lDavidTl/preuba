2257ed7628af7802a7a0f866747b791ed30716c1
uriel <urielch997@gmail.com>
2023-01-10 15:52:29 -0800

    Se realizo cambio

--- src/main/java/com/example/pruebaProducto/DemoApplication.java
+++ src/main/java/com/example/pruebaProducto/DemoApplication.java
@@ -2,12 +2,8 @@ package com.example.pruebaProducto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.example.*" })
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/pruebaProducto/model/Producto.java
+++ src/main/java/com/example/pruebaProducto/model/Producto.java
@@ -2,12 +2,12 @@ package com.example.pruebaProducto.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
 
 
 @Entity
